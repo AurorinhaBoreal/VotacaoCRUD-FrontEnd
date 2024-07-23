c52422cb24e5f656dba6b9d8a8844485deef0062
Aurora Kruschewsky <AurorinhaBoreal@users.noreply.github.com>
2024-07-23 10:54:37 -0300

    refactor: Clear forms and implement link to active agendas

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -5,6 +5,7 @@ import {InfoIcon} from "@chakra-ui/icons"
 import { useState, ChangeEvent } from "react";
 import CreateUserDTO from "../../types/CreateUserDTO";
 import userService from "../../service/userService";
+import { Link } from "react-router-dom";
 
 export default function SignUp() {
   const [formData, setFormData] = useState<CreateUserDTO>({
@@ -23,10 +24,18 @@ export default function SignUp() {
     console.log(formData)
   }
 
-  const handleSubmit = () => {
-    console.log("Button Clicked!")
-    console.log(formData.userType)
-    userService.createUser(formData);
+  const handleSubmit = async () => {
+    try {
+      await userService.createUser(formData);
+      setFormData({
+        firstName: "",
+        surname: "",
+        userType: "",
+        cpf: "",
+      })
+    } catch (error) {
+
+    }
   }
 
   return (
@@ -44,11 +53,11 @@ export default function SignUp() {
           <FormControl className={styles.forms}>
             <Box>
               <FormLabel display={"flex"} justifyContent={"center"} margin={0} fontSize={"1.5vw"} opacity={0.8}>First Name:</FormLabel>
-              <Input type="text" name="firstName" value={formData.firstName} onChange={handleChange} bg={"var(--c-gray2)"} color={"var(--c-black)"} textAlign={"center"} fontSize={"1.8rem"} padding={0} width={60}/>
+              <Input type="text" name="firstName" value={formData.firstName} onChange={handleChange} m={5} bg={"var(--c-gray2)"} color={"var(--c-black)"} textAlign={"center"} fontSize={"1.8rem"} padding={0} width={60}/>
             </Box>
             <Box>
               <FormLabel display={"flex"} justifyContent={"center"} margin={0} fontSize={"1.5vw"} opacity={0.8}>Surname:</FormLabel>
-              <Input type="text" name="surname" value={formData.surname} onChange={handleChange} bg={"var(--c-gray2)"} color={"var(--c-black)"} textAlign={"center"} fontSize={"1.8rem"} padding={0} width={60}/>
+              <Input type="text" name="surname" value={formData.surname} onChange={handleChange} m={5} bg={"var(--c-gray2)"} color={"var(--c-black)"} textAlign={"center"} fontSize={"1.8rem"} padding={0} width={60}/>
             </Box>
             <Box>
               <FormLabel display={"flex"} justifyContent={"center"} margin={0} fontSize={"1.5vw"} opacity={0.8}>
@@ -57,26 +66,30 @@ export default function SignUp() {
                   <InfoIcon boxSize={6} ml={2} color="black" justifyContent="center" alignSelf={"center"}/>
                 </Tooltip>
               </FormLabel>
-              <Input name="cpf" value={formData.cpf} onChange={handleChange} type="text" bg={"var(--c-gray2)"} color={"var(--c-black)"} textAlign={"center"} fontSize={"1.5rem"} padding={0} width={60}/>
+              <Input name="cpf" value={formData.cpf} onChange={handleChange} type="text" m={5} bg={"var(--c-gray2)"} color={"var(--c-black)"} textAlign={"center"} fontSize={"1.5rem"} padding={0} width={60}/>
             </Box>
             <Box>
               <FormLabel display={"flex"} justifyContent={"center"} margin={0} fontSize={"1.5vw"} opacity={0.8}>Select Role:</FormLabel>
-              <Select name="userType" value={formData.userType} onChange={handleChange} placeholder='-' bg={"var(--c-gray2)"} color={"var(--c-black)"} textAlign={"center"} fontSize={"1.5rem"} padding={0} width={60}>
+              <Select name="userType" value={formData.userType} onChange={handleChange} placeholder='-' m={5} bg={"var(--c-gray2)"} color={"var(--c-black)"} textAlign={"center"} fontSize={"1.5rem"} padding={0} width={60}>
                 <option value={"C"}>Common</option>
                 <option value={"A"}>Administrator</option>
               </Select>
             </Box>
-            <Button
-              mt={10}
-              fontSize="1.5rem"
-              color="mono.500"
-              bg="main.200"
-              type='submit'
-              fontWeight={700}
-              onClick={handleSubmit}
-            >
-              Add to Database
-            </Button>
+            <Box display={"flex"} flexDirection={"column"} alignItems={"center"} mb={4}>
+              <Button
+                mb={2}
+                ml={8} mr={8}
+                fontSize="1.5rem"
+                color="mono.500"
+                bg="main.200"
+                type='submit'
+                fontWeight={700}
+                onClick={handleSubmit}
+              >
+                Add to Database
+              </Button>
+              <Link to={"/agenda-active"}>Or go to Active Agendas</Link>
+            </Box>
           </FormControl>
         </Box>
         <Box className={styles.secondaryInfo}>
