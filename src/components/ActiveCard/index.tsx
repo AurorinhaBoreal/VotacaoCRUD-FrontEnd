4aff4d87c22f9fd037154c9957ee0f69907ea743
Aurora Kruschewsky <AurorinhaBoreal@users.noreply.github.com>
2024-08-15 14:56:38 -0300

    fix: Match Vote String with Enum in BackEnd

--- src/components/ActiveCard/index.tsx
+++ src/components/ActiveCard/index.tsx
@@ -59,8 +59,8 @@ export default function ActiveCard({agenda}: info) {
         <Box width={"100%"} display={"flex"} flexDirection={"column"} alignItems={"center"}>
           <Input className={styles.cpfInput} onChange={handleChange} value={cpf} type="number" padding={0} textAlign={"center"} borderRadius={10} placeholder="CPF" bg={"var(--c-gray1)"} color={"black"}/>
           <Box className={styles.buttonWrapper}>
-            <Button className={styles.voteButton} onClick={() => handleVote("Y")} bg={"var(--c-green)"} _hover={{ bg: "main.350" }}>YES</Button>
-            <Button className={styles.voteButton} onClick={() => handleVote("N")} bg={"var(--c-red)"} _hover={{ bg: "main.450" }}>NO</Button>
+            <Button className={styles.voteButton} onClick={() => handleVote("YES")} bg={"var(--c-green)"} _hover={{ bg: "main.350" }}>YES</Button>
+            <Button className={styles.voteButton} onClick={() => handleVote("NO")} bg={"var(--c-red)"} _hover={{ bg: "main.450" }}>NO</Button>
           </Box>
         </Box>
     </Box>
