a47b8a40a3519f151ef1c3032e4f946276039095
Aurora Kruschewsky <AurorinhaBoreal@users.noreply.github.com>
2024-09-24 17:24:37 -0300

    test: Refactor Footer Test

--- src/components/Footer/footer.test.tsx
+++ src/components/Footer/footer.test.tsx
@@ -2,9 +2,9 @@ import {render, screen} from "@testing-library/react"
 import {describe, expect, test} from "vitest"
 import Footer from "./index";
 
-describe('Testa o componente do Footer', () => {
+describe('Test the footer component', () => {
 
-    test("Deve verificar o texto", () => {
+    test("Should verify the text", () => {
         render(<Footer/>)
         const textElement = screen.getByText("2024 Made by Aurora Kruschewsky", {exact: false})
         expect(textElement).toBeTruthy()
