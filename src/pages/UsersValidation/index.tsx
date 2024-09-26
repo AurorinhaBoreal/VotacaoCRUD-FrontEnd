48f66530126a7a915ca17bf6867fbcde12fba797
Aurora Kruschewsky <AurorinhaBoreal@users.noreply.github.com>
2024-09-26 11:43:03 -0300

    test: Implement UsersValidation Test

--- src/pages/UsersValidation/index.tsx
+++ src/pages/UsersValidation/index.tsx
@@ -51,7 +51,7 @@ export default function UsersValidation() {
                 <Box className={styles.infoContainer} width={"fit-content"} mb={"2vw"}>
                     <Text className={styles.title} data-cy="title-V">Validation</Text>
                     <Text className={styles.description}>Please inform your cpf to be validated and access users page.</Text>
-                    <Input className={styles.cpfInput} data-cy="inputUV-CPF" as={InputMask} mask={"999.999.999-99"} maskChar={null} onChange={handleChange} value={cpf} type="text" padding={0} textAlign={"center"} borderRadius={10} placeholder="CPF" bg={"mono.300"} color={"black"}/>
+                    <Input className={styles.cpfInput} data-cy="inputUV-CPF" as={InputMask} mask={"999.999.999-99"} maskChar={null} onChange={handleChange} value={cpf} alt="CPF Input" type="text" padding={0} textAlign={"center"} borderRadius={10} placeholder="CPF" bg={"mono.300"} color={"black"}/>
                     <Button className={styles.validateBtn}
                         data-cy="buttonVal-U"
                         color="mono.500"
---
+++ src/pages/UsersValidation/usersValidation.test.tsx
@@ -0,0 +1,32 @@
+import { render, screen, fireEvent } from "@testing-library/react";
+import { describe, test } from "vitest";
+import UsersValidation from ".";
+import { expect } from "vitest";
+import { BrowserRouter } from "react-router-dom";
+
+describe("Test userValidation Page", () => {
+    test("Test if component renderize", () => {
+        render(
+            <BrowserRouter>
+                <UsersValidation/>
+            </BrowserRouter>
+        )
+
+        const title = screen.getByText("Validation")
+        const description = screen.getByText("Please inform your cpf to be validated and access users page.")
+        expect(title).toBeTruthy()
+        expect(description).toBeTruthy()
+    })
+
+    test("Verify if input works", () => {
+        render(
+            <BrowserRouter>
+                <UsersValidation/>
+            </BrowserRouter>
+        )
+        const inputCPF = screen.getByAltText("CPF Input") as HTMLInputElement
+
+        fireEvent.change(inputCPF, { target: { value: "12345678910" } });
+        expect(inputCPF.value).toBe("123.456.789-10");
+    })
+})
